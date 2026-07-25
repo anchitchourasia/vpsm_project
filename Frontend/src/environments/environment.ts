1ebb08124132056f59b066584537efb39ac1ba38
Aditya Pathak <aditya.pathak@example.com>
2026-07-25 13:53:56 +0530

    ad

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,8 +1,19 @@
+// export const environment = {
+//   production: false,
+//   apiBaseUrl : 'http://192.168.9.130:3031/vpms',
+//  // apiBaseUrl : 'http://localhost:8076/vpms',
+//   apiKey     : 'VPMS_SECRET_KEY_2026',
+//   cvpsBaseUrl: 'http://192.168.9.130:3030/cvps'
+  
+// };
+
+
 export const environment = {
   production: false,
-  apiBaseUrl : 'http://192.168.9.130:3031/vpms',
- // apiBaseUrl : 'http://localhost:8076/vpms',
+  apiBaseUrl : 'http://localhost:4032/vpms',
+ // apiBaseUrl : 'http://:8076/vpms',
   apiKey     : 'VPMS_SECRET_KEY_2026',
-  cvpsBaseUrl: 'http://192.168.9.130:3030/cvps'
+  cvpsBaseUrl: 'http://localhost:3030/cvps'
   
 };
+
