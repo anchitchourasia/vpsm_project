42a82fa5e7c0fdcc7fb906438e1ae82f845b110f
anchit <anchitchourasia78@gmail.com>
2026-08-04 10:55:09 +0530

    2

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   // apiBaseUrl : 'http://192.168.8.28:4032/vpms',
   // apiBaseUrl: 'http://192.168.9.130:3031/vpms',
-  apiBaseUrl: 'http://localhost:8090/vpms',
+  apiBaseUrl: 'http://localhost:3031/vpms',
   // apiBaseUrl : 'http://localhost:8076/vpms',
   apiKey: 'VPMS_SECRET_KEY_2026',
   cvpsBaseUrl: 'http://192.168.9.130:3030/cvps'
